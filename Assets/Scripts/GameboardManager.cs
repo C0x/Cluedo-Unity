bda1b6b159240742f544bbd591e21b38f31efc72
jclaessens97 <jclaessens97@hotmail.com>
2017-05-09 21:21:12 +0200

    Marked doortiles, secret entrances & spawnpoints

--- Assets/Scripts/GameboardManager.cs
+++ Assets/Scripts/GameboardManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class GameboardManager : MonoBehaviour 
 {
     private const float TILE_SIZE = .8f;
-    private const float TILE_OFFSET = TILE_SIZE / 2;
+    private const float TILE_OFFSET = 0.5f;
 
     private const int NUMBER_OF_TILES_X = 25;
     private const int NUMBER_OF_TILES_Y = 25;
@@ -13,21 +13,32 @@ public class GameboardManager : MonoBehaviour
     private int selectionX = -1;
     private int selectionY = -1;
 
-    void Update() 
-    {
-        DrawGameBoard();
+    public List<GameObject> SpawnPoints;
+    public List<GameObject> AccessibleTiles;
+
+    void Start() 
+    {        
     }
 
+    void Update() 
+    {
+        DrawGameBoard(); //debug purpose only
+    }
+
+    ///<summary>
+    /// Debug method to draw lines of the gameboard in the inspector
+    ///<summary/>
     private void DrawGameBoard() 
     {
         Vector3 widthLine = Vector3.right * NUMBER_OF_TILES_X;
         Vector3 heightLine = Vector3.forward * NUMBER_OF_TILES_Y;
 
-        for (int i = 0; i <= NUMBER_OF_TILES_Y; i++) 
+        for (int i = 0; i < NUMBER_OF_TILES_Y; i++) 
         {
             Vector3 start = Vector3.forward * i;
             Debug.DrawLine(start, start + widthLine);
-            for (int j = 0; j <= NUMBER_OF_TILES_X; j++) 
+
+            for (int j = 0; j < NUMBER_OF_TILES_X; j++) 
             {
                 start = Vector3.right * j;
                 Debug.DrawLine(start, start + heightLine);
