e0fa3487b7d903f5c3459ce9e17b44c1358e35d8
jclaessens97 <jclaessens97@hotmail.com>
2017-05-09 22:34:50 +0200

    Programmatically detect 'default tiles' and mark them

--- Assets/Scripts/GameboardManager.cs
+++ Assets/Scripts/GameboardManager.cs
@@ -13,16 +13,72 @@ public class GameboardManager : MonoBehaviour
     private int selectionX = -1;
     private int selectionY = -1;
 
+    public Transform InvisibleDefaultTilePrefab;
     public List<GameObject> SpawnPoints;
     public List<GameObject> AccessibleTiles;
 
     void Start() 
-    {        
+    {
+        InitializeDefaultTiles();        
     }
 
     void Update() 
     {
         DrawGameBoard(); //debug purpose only
+    }
+
+    private void InitializeDefaultTiles()
+    {
+        for (int i = 0; i < NUMBER_OF_TILES_Y; i++) 
+        {
+            for (int j = 0; j < NUMBER_OF_TILES_X; j++) 
+            {
+                #region skip loop for rooms
+
+                if (j >= 0 && j <= 5 && i >= 0 && i <= 7) continue;         //lounge
+                if (j >= 9 && j <= 14 && i >= 0 && i <= 8) continue;        //diner
+                if (j == 15 && i >= 0 && i <= 4) continue;                  //diner
+                if (j >= 18 && j <= 23 && i >= 0 && i <= 5) continue;       //kitchen
+                if (j >= 0 && j <= 7 && i >= 10 && i <= 15) continue;       //hall   
+                if (j >= 17 && j <= 22 && i >= 8 && i <= 16) continue;      //ball
+                if (j == 23 && i >= 11 && i <= 13) continue;                //ball
+                if (j >= 1 && j <= 3 && i == 18) continue;                  //study
+                if (j >= 0 && j <= 3 && i >= 19 && i <= 24) continue;       //study
+                if ((j >= 7 && j <= 9) && (i == 18 || i == 24)) continue;   //library
+                if (j >= 6 && j <= 10 && i >= 19 && i <= 23) continue;      //library
+                if (j >= 12 && j <= 16 && i >= 19 && i <= 24) continue;     //billiart
+                if ((j >= 20 && j <= 23) && (i == 19 || i == 24)) continue; //conservatory
+                if (j >= 19 && j <= 23 && i >= 20 && i <= 23) continue;     //conservatory          
+
+                #endregion
+
+                #region skip loop for spawns
+
+                if (j == 24 && i == 9) continue;    //white
+                if (j == 24 && i == 15) continue;   //green
+                if (j == 18 && i == 24) continue;   //blue
+                if (j == 7 && i == 0) continue;     //yellow
+                if (j == 5 && i == 24) continue;    //purple
+                if (j == 0 && i == 8) continue;     //red
+                               
+                #endregion
+
+                #region skip loop for outside tiles (including cellar)
+
+                if (i == 0 && j != 17) continue;
+                if (j == 24) continue;
+                if (i == 24) continue;
+                if (i != 17 && j == 0) continue;
+                if ((i == 6 || i == 10 || i == 14 || i == 18) && j == 23) continue;
+                if (j >= 9 && j <= 14 && i >= 11 && i <= 15) continue; //cellar
+
+                #endregion
+
+                Transform tile = (Transform) Instantiate(InvisibleDefaultTilePrefab, new Vector3(i + TILE_OFFSET, 0.7f, j + TILE_OFFSET), Quaternion.identity);
+                tile.name = "DefaultTile" + i + "-" + j;
+                tile.parent = this.transform;
+            }
+        }
     }
 
     ///<summary>
