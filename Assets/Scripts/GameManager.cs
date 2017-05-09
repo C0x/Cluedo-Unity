82290f4d2065e77323647f0e485e974e9bae53ce
jclaessens97 <jclaessens97@hotmail.com>
2017-05-09 23:20:35 +0200

    Spawn individual pawns programmatically

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public Pawn WhitePrefab;
+	public Pawn GreenPrefab;
+	public Pawn BluePrefab;
+	public Pawn PurplePrefab;
+	public Pawn YellowPrefab;
+	public Pawn RedPrefab;
+
+	private List<Pawn> PawnsInGame;
+
+	void Start()
+	{
+		SpawnPawns();
+	}
+
+	private void DebugSetup()
+	{
+		//debug
+		PawnsInGame = new List<Pawn>();
+		PawnsInGame.Add(WhitePrefab);
+		PawnsInGame.Add(GreenPrefab);
+		PawnsInGame.Add(BluePrefab);
+		PawnsInGame.Add(PurplePrefab);
+		PawnsInGame.Add(YellowPrefab);
+		PawnsInGame.Add(RedPrefab);
+	}
+
+	private void SpawnPawns() 
+	{
+		DebugSetup();
+
+		foreach (var pawn in PawnsInGame)
+		{
+			Pawn p = Instantiate(pawn, pawn.transform.position, pawn.transform.rotation);
+		}
+	}
+
+
+}
