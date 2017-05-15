9b24630da6d06dbfd2c0bacb1565dcdd29967b0a
jclaessens97 <jclaessens97@hotmail.com>
2017-05-15 13:22:19 +0200

    Created MainMenu & organized scripts folder

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+	public void StartGame()
+	{
+
+	}
+
+	public void Credits()
+	{
+		
+	}
+
+	public void Quit()
+	{
+		Debug.Log("Quit");
+		Application.Quit();
+	}
+
+}
