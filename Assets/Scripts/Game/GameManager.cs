558ebcddde1fe073ffa556672d3283c7fb7a20be
jclaessens97 <jclaessens97@hotmail.com>
2017-05-15 17:49:46 +0200

    Loads GUI with values from startgame

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -13,12 +13,36 @@ public class GameManager : MonoBehaviour {
 
 	private PersistentData persistentData;
 	private int currentIndex;
+
+	private Player[] players;
 	private int numberOfPlayers;
 
 
 	void Start () {
+		LoadPersistentData();
+		InitGui();
+
+		currentIndex = 0;
+		ToggleCurrentPlayerPanels();
+	}
+
+	void Update()
+	{
+		//debug
+		if (Input.GetKeyDown("space"))
+		{
+			ToggleCurrentPlayerPanels();
+		}
+
+	}
+
+	///<summary>
+	///	Loads persistent data into local player array
+	///</summary>
+	private void LoadPersistentData()
+	{
 		GameObject persistentGO = GameObject.Find("PersistentData");
-		Player[] players = null;
+		players = null;
 
 		if (Debug.isDebugBuild && persistentGO == null)
 		{
@@ -28,7 +52,7 @@ public class GameManager : MonoBehaviour {
 		{
 			persistentData = persistentGO.GetComponent<PersistentData>();
 			players = persistentData.Players;
-			//TODO: destroy??
+			Destroy(persistentGO, 1f);
 		}
 
 		numberOfPlayers = players.Length;
@@ -37,33 +61,67 @@ public class GameManager : MonoBehaviour {
 		{
 			Debug.Log(players[i].ToString());
 		}
-
-		currentIndex = 0;
 	}
 
-	void Update()
+	///<summary>
+	///	Loads GUI with values of persistent data
+	///</summary>
+	private void InitGui()
 	{
-
-		if (Input.GetKeyDown("space"))
+		//Hide unused images
+		for (int i = numberOfPlayers; i < Images.Count; i++)
 		{
-			ToggleCurrentPlayerPanels();
+			Images[i].enabled = false;
 		}
 
+		for (int i = 0; i < numberOfPlayers; i++)
+		{
+			GameObject t1GO = GameObject.Find("PlayerName" + (i + 1));
+			t1GO.GetComponent<Text>().text = players[i].Name;
+			if (players[i].IsCpu)
+			{
+				t1GO.GetComponent<Text>().text += "\t(CPU)";
+			}
+
+			GameObject t2GO = GameObject.Find("PlayingAs" + (i + 1));
+
+			if (players[i].Color == Color.white)
+			{
+				t2GO.GetComponent<Text>().text = "Mrs. White";
+			} 
+			else if (players[i].Color == Color.green)
+			{
+				t2GO.GetComponent<Text>().text = "Mr. Green";
+			} 
+			else if (players[i].Color == Color.blue)
+			{
+				t2GO.GetComponent<Text>().text = "Mrs. Peacock";
+			} 
+			else if (players[i].Color == Color.yellow)
+			{
+				t2GO.GetComponent<Text>().text = "Col. Mustard";
+			}
+			else if (players[i].Color == Color.magenta)
+			{
+				t2GO.GetComponent<Text>().text = "Prof. Plum";
+			}
+			else if (players[i].Color == Color.red)
+			{
+				t2GO.GetComponent<Text>().text = "Mrs. Scarlett";
+			}	
+		}
 	}
 
-	private void InitGUI()
-	{
-
-	}
-
+	///<summary>
+	///	Show panel of current player
+	///</summary>
 	private void ToggleCurrentPlayerPanels()
 	{
 		if (currentIndex == numberOfPlayers)
 		{
 			currentIndex = 0;
 			CollapsablePanels[numberOfPlayers - 1].alpha = 0;
-		}
-					
+		}					
 
 		Debug.Log("CurrentIndex: " + currentIndex);
 
@@ -80,6 +138,9 @@ public class GameManager : MonoBehaviour {
 		currentIndex++;
 	}
 
+	///<summary>
+	///	Hides all panels (of current players)
+	///</summary>
 	private void CollapseAllPanels()
 	{
 		CollapsablePanels.ForEach(p => p.alpha = 0);
